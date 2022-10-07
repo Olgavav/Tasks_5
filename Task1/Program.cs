3160f4a94df2023d6268646636354769b9c6ea49
Olgavav <petrova-olga@myrambler.ru>
2022-10-07 11:20:11 +0400

    Задача 34

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Clear();
+int[] array = new int[6];
+FillArray(array);
+Evennumbers(array);
+
+    
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < 6; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+}
+
+
+void Evennumbers(int[] array)
+{ 
+    int n = 0; 
+    for (int i = 0; i < 6; i++)
+    {   Console.Write($"{array[i]}, ");
+        if (array[i] % 2 == 0)
+        
+        {
+           n++;
+        }
+    
+    }
+Console.Write($" Колличество четных чисел в мвссиве  = {n}");
+}
